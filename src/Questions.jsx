ed05ad23373d6d7b2fb8a7001fdda72767823f25
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2023-08-17 22:42:12 +0530

    first commit

---
+++ src/Questions.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SingleQuestion from "./SingleQuestion";
+
+const Questions = ({questions, activeId ,toggleQuestion}) => {
+    
+    return(
+        <section className="container">
+            <h1>FAQ section</h1>
+            {questions.map((question)=>{
+             return( 
+                <SingleQuestion 
+                    key={question.id} 
+                    {...question}
+                    activeId={activeId} 
+                    toggleQuestion={toggleQuestion}
+                    />
+                )
+            })}
+        </section>
+    )
+};
+
+export default Questions;
